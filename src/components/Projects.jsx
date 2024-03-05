56e6e0a72246c528e21825f952238957c57a9eca
Nana <frimpongnana8@gmail.com>
2024-03-05 19:21:38 +0000

    update

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
  import './Project.css'
 
+
+ 
  
 function Projects(props) {
    console.log(props)
   return (
     
+    
        <div className="card"  >
-    <img src={props.image} className="card-img-top" alt="..." />
+    <img src={props.image} className="card-img-top "  alt="..." />
      <div className="card-body">
       <h5 className="card-title text-danger">{props.title}</h5>
       <a href={props.github} target='_blank' className="btn btn-primary">Github page</a>
